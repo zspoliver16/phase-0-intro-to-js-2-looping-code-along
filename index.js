c3831ac7ed6086f2f28289660fd4957103356a96
zachary oliver <zspoliver@gmail.com>
2024-03-20 20:25:18 -0400

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+ function writeCards(arrayOfNames, eventName){
+      const messages = []
+    for(let i = 0; i < arrayOfNames.length; i++){
+
+    messages.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+
+  }
+
+  function countDown(number) {
+
+    while(number >= 0) {
+      console.log(number--)
+    }
+  }
